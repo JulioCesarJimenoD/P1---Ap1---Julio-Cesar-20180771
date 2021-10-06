bd284968e246d817b7d0f3f9e7ee6ee5c6ffa619
Julio Cesar Jimeno Disla <julio_jimeno@ucne.edu.do>
2021-10-05 21:48:57 -0400

    Creacion de Entidades

---
+++ Entidades/Aportes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1___Ap1___Julio_Cesar_20180771.Entidades
+{
+    public class Aportes
+    {
+        [Key]
+        public int AporteID { get; set; }
+        public string Persona { get; set; }
+        public string  Concepto { get; set; }
+        public string monto { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+    }
+}
