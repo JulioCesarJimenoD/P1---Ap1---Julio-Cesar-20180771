6390320c3875c0cf078e90a45ceadc8585a33bb1
Julio Cesar Jimeno Disla <julio_jimeno@ucne.edu.do>
2021-10-05 23:32:50 -0400

    Creando BLL Utlidades

---
+++ BLL/UtilidadesBLL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1___Ap1___Julio_Cesar_20180771.BLL
+{
+    public class UtilidadesBLL
+    {
+        public static int ToInt(String valor)
+        {
+            int retorno = 0;
+            int.TryParse(valor, out retorno);
+        }
+    }
+}
