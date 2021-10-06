e03baf6ef08db5bf6e64c5fe3ae3067136125b16
Julio Cesar Jimeno Disla <julio_jimeno@ucne.edu.do>
2021-10-05 23:30:23 -0400

    Creando BLL Aporte

---
+++ BLL/AporteBLL.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P1___Ap1___Julio_Cesar_20180771.DAL;
+using Microsoft.EntityFrameworkCore;
+using P1___Ap1___Julio_Cesar_20180771.UI.Registro;
+using P1___Ap1___Julio_Cesar_20180771.Entidades;
+
+
+namespace P1___Ap1___Julio_Cesar_20180771.BLL
+{
+   public class AporteBLL
+    {
+        public static bool Existente(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool existente = false;
+            try
+            {
+                existente = contexto.Aportes.Any(async => async.AporteID == id);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return existente;
+
+        }
+
+        public static bool Insertar (Aportes aportes)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+            try
+            {
+                contexto.Aportes.Add(aportes);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Modificiar(Aportes aportes)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                contexto.Entry(aportes).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+      
+
+        public static bool Eliminar(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                var aportes = contexto.aportes.Find(id);
+                if (aportes != null)
+                {
+                    contexto.aportes.Remove(aportes);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        public static bool Guardar(Aportes aportes)
+        {
+            if (!Existente(aportes.AporteID))
+            {
+                return Insertar(aportes);
+            }
+            else
+            {
+                return Modificiar(aportes);
+            }
+        }
+
+        public static Aportes Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Aportes aportes;
+
+            try
+            {
+                aportes = contexto.aportes.Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return aportes;
+        }
+
+        public static List <Aportes> GetList(Expression<Func<Aportes, bool>> cristerio)
+        {
+            List<Aportes> listado = new List<Aportes>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                listado = contexto.aportes.Where(cristerio).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return listado;
+        }
+
+        public static List<Aportes> GetAportes()
+        {
+            List<Aportes> listados = new List<Aportes>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                listados = contexto.Aportes.ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }return listados;
+        }
+    }
+}
---
+++ UI/Registro/rRegistroAportes.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P1___Ap1___Julio_Cesar_20180771.UI.Registro
+{
+    /// <summary>
+    /// Interaction logic for rRegistroAportes.xaml
+    /// </summary>
+    public partial class rRegistroAportes : Window
+    {
+        public rRegistroAportes()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NuevoButto_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
