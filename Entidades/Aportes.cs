53cf9e636bae9827564e62077a6d76104ca04731
Julio Cesar Jimeno Disla <julio_jimeno@ucne.edu.do>
2021-10-11 19:23:26 -0400

    Cambiar propiedades de la Entidad aportes. Ya fue cambiada.

--- Entidades/Aportes.cs
+++ Entidades/Aportes.cs
@@ -10,7 +10,7 @@ namespace P1___Ap1___Julio_Cesar_20180771.Entidades
     public class Aportes
     {
         [Key]
-        public int AporteID { get; set; }
+        public int AporteId{ get; set; }
         public string Persona { get; set; }
         public string  Concepto { get; set; }
         public float Monto { get; set; }
